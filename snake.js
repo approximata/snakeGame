8abd1ff8a09a4b383a6e6ec8d188f03e4369f6cd
Mate Bodor <bodormate@gmail.com>
2017-05-25 08:09:52 +0200

    rewrite to multiplayer

--- snake.js
+++ snake.js
@@ -1,5 +1,6 @@
 'use strict';
 var canvas = document.querySelector('.snakeCanvas');
+var check = document.querySelector('input[type=checkbox]');
 var BLOCK = 15;
 var SPEED = 150;
 canvas.width = window.innerWidth - (window.innerWidth % BLOCK + BLOCK * 2);
@@ -13,13 +14,10 @@ function Snake(x, y, keys) {
   this.dx = 1;
   this.dy = 0;
   this.body = [[]];
-  this.isEat = function(food){
-    console.log("food " + food[0] +" "+ food[1]);
-    console.log("snake" + this.x +" "+ this.y);
-
-    return this.x === food[0] && this.y === food[1]
+  this.isEat = function(point) {
+    return this.x === point[0] && this.y === point[1]
   };
-  this.grow = function(){
+  this.grow = function() {
     this.body.push([this.x, this.y]);
   };
   this.setBody = function() {
@@ -30,10 +28,22 @@ function Snake(x, y, keys) {
     this.x += this.dx * BLOCK;
     this.y += this.dy * BLOCK;
     this.setBody()
-    console.log(this.x);
-    for(var i =0; i < this.body.length; i++){
+    for(var i = 0; i < this.body.length; i++){
       contex.fillRect(this.body[i][0], this.body[i][1], BLOCK, BLOCK);
     }
+  };
+  this.isDead = function() {
+    for(var i = 1; i < this.body.length; i++) {
+      if(this.isEat(this.body[i])) {
+        return true;
+      }
+    }
+    return false;
+  };
+  this.reset = function() {
+    this.body = [[]];
+    this.dx = 1;
+    this.dy = 0;
   };
   this.control = function(e, keys){
     this.move(keys[e.keyCode])
@@ -63,36 +73,44 @@ function Snake(x, y, keys) {
   window.addEventListener('keydown', function(e){this.control(e, this.keys)}.bind(this));
 }
 
-function Board(snake) {
-  this.snake = snake;
+function Board(snakes) {
+  this.snakes = snakes;
   this.food = [];
   this.clear = function() {
    contex.clearRect(0, 0, canvas.width, canvas.height);
  }
- this.border = function(x, y) {
-   if(x > canvas.width - BLOCK){
-     this.snake.x = 0 - BLOCK;
-   }
-   if(x < 0){
-     this.snake.x = canvas.width;
-   }
-   if(y > canvas.height - BLOCK){
-     this.snake.y = 0 - BLOCK;
-   }
-   if(y < 0){
-     this.snake.y = canvas.height;
-   }
+ this.border = function() {
+  for(var i = 0; i < this.snakes.length; i++){
+    if(this.snakes[i].x > canvas.width - BLOCK){
+      this.snakes[i].x = 0;
+    }
+    if(this.snakes[i].x < 0){
+      this.snakes[i].x = canvas.width;
+    }
+    if(this.snakes[i].y > canvas.height - BLOCK){
+      this.snakes[i].y = 0;
+    }
+    if(this.snakes[i].y < 0){
+      this.snakes[i].y = canvas.height;
+    }
+  }
  };
  this.setFood = function() {
      var x = Math.floor((Math.random() * canvas.width) / BLOCK) * BLOCK;
      var y = Math.floor((Math.random() * canvas.height) / BLOCK) * BLOCK;
      this.food = [x, y]
  };
+
  this.draw = function() {
-   if(this.snake.isEat(this.food)){
-     console.log('reset');
-     this.snake.grow();
-     this.setFood();
+   for(var i = 0; i < this.snakes.length; i++){
+     this.snakes[i].draw();
+     if(this.snakes[i].isDead()) {
+       this.snakes[i].reset();
+     }
+     if(this.snakes[i].isEat(this.food)){
+       this.snakes[i].grow();
+       this.setFood();
+     }
    }
    contex.fillRect(this.food[0], this.food[1], BLOCK, BLOCK);
  };
@@ -105,17 +123,38 @@ var keys1 = {
   40:'down'
 };
 
-var mySnake = new Snake(0, 0, keys1);
-var myBoard = new Board(mySnake);
+var keys2= {
+  68:'right',
+  65:'left',
+  87:'up',
+  83:'down'
+}
 
+var mySnake1 = new Snake(0, 0, keys1);
+var mySnake2 = new Snake(BLOCK, BLOCK, keys2);
+
+var mySnakes2 = [mySnake1, mySnake2];
+var mySnakes1 = [mySnake1]
+
+var myBoard = mySnakes2;
+
+// function setBoard(){
+//   console.log(check.checked);
+//   if(check.checked){
+//     myBoard = new Board(mySnakes2);
+//   }
+//   else{
+//     myBoard = new Board(mySnakes1);
+//   }
+// }
+
+// setBoard();
 myBoard.setFood();
 
 function gameLoop(){
   myBoard.clear();
-  myBoard.border(mySnake.x, mySnake.y)
-  mySnake.draw();
+  myBoard.border();
   myBoard.draw();
 }
-
 
 setInterval(function(){gameLoop()}, SPEED);
